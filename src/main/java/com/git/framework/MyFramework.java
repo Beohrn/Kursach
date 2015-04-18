2ed60af6c33498250e93b56143c4d35d87d60604
Alex <beohrn1@gmail.com>
2015-04-18 10:58:36 +0300

    crate project

---
+++ src/main/java/com/git/framework/MyFramework.java
@@ -0,0 +1,12 @@
+package com.git.framework;
+
+/**
+ * Created by Alexander on 18.04.2015.
+ */
+public class MyFramework {
+
+    public static void makeAllProject() {
+        System.out.println("Success!");
+    }
+
+}
